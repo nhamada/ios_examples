28e5df53d81aad8d664cc15e9e2f4e9c65948ba9
Naohiro Hamada <nao.pinguinus@icloud.com>
2024-11-16 14:38:02 +0900

    Create example app

---
+++ Location/Sources/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  app
+//
+//  Created by Naohiro Hamada on 2024/11/16.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
